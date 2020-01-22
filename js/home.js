fa238353eb42615900a2320d71a8be9ec970b1c5
so77 <59861497+so77@users.noreply.github.com>
2020-01-22 15:56:18 +0900

    Update home.js

--- js/home.js
+++ js/home.js
@@ -15,7 +15,7 @@ $(function(){
     selector: '.background',
     sizeVariations: 10,
     color: ['#00bbdd', '#404B69', '#DBEDF3'],
-    connectParticles: false
+    connectParticles: true
     });
 
  
